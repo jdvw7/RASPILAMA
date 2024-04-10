680de6fa86f8be1173ad89811cfee68123b29695
jdvw7 <166649319+jdvw7@users.noreply.github.com>
2024-04-10 21:34:19 +0100

    Delete llama2.py

--- llama2.py
+++
@@ -1,5 +0,0 @@
-import sys
-from llama_cpp import Llama
-llm = Llama(model_path="tinyllama-1.1b-chat-v1.0.Q4_K_M.gguf", verbose=False)
-output = llm("<user>\n" + sys.argv[1] + "\n<assistant>\n", max_tokens=40)
-print(output['choices'][0]["text"] + "...")
